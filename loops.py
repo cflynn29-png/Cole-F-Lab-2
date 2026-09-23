38bf8e500cea76ecba9e0063da4d8791242d8dfd
cflynn29-png <cflynn29@setoncatholic.org>
2026-09-23 17:45:04 +0000

    final code

---
+++ loops.py
@@ -0,0 +1,14 @@
+z = int(input("what year were you born?"))
+
+y = int(input("what month were you born?"))
+
+x = int(input("what day were you born?"))
+
+if x > 20:
+    print("you were born on a Tuesday")
+
+elif x < 10:
+    print("you were born on a Wednesday") 
+
+else:
+    print("you were born on a Friday")
